5702505e42d65711b612be8a03901207f9ab6ed1
Gabriel <ironhammer354@gmail.com>
2014-01-05 13:34:06 -0800

    Fixed Proxy Refrence

--- common/com/ironhammer354/mythic/lib/Reference.java
+++ common/com/ironhammer354/mythic/lib/Reference.java
@@ -5,7 +5,7 @@ public class Reference {
 	public static final String MOD_ID = "mythic";
 	public static final String MOD_NAME = "Mythic";
 	public static final String MOD_VERSION = "0.0.0.0";
-	public static final String CLIENT_PROXY = "com.mythic.core.proxy.ClientProxy";
-	public static final String COMMON_PROXY = "com.mythic.core.proxy.CommonProxy";
+	public static final String CLIENT_PROXY = "com.ironhammer354.mythic.core.proxy.ClientProxy";
+	public static final String COMMON_PROXY = "com.ironhammer354.mythic.core.proxy.CommonProxy";
 	public static final int VANILLA_ITEMID_FIX = 256;
 }
