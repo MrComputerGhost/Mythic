5bd0feb57a86dda1324a95c865443027c31d4675
Gabriel <ironhammer354@gmail.com>
2014-02-03 09:04:57 -0800

    Making Pretty Blocks! Part:2 W/kkaylium
    Thank-You, kkaylium, Your the best!

--- common/com/ironhammer354/mythic/block/WitherBlock.java
+++ common/com/ironhammer354/mythic/block/WitherBlock.java
@@ -44,34 +44,58 @@ public class WitherBlock extends Block {
         return unlocalizedName.substring(unlocalizedName.indexOf(".") + 1);
     }
 	
-	public void randomDisplayTick(World par1World, int par2, int par3, int par4, Random par5Random)
+	public void randomDisplayTick(World world, int par2, int par3, int par4, Random random)
     {
-		int l = par1World.getBlockMetadata(par2, par3, par4);
-        float f = (float)par2 + 0.5F;
-        float f1 = (float)par3 + 0.0F + par5Random.nextFloat() * 6.0F / 16.0F;
-        float f2 = (float)par4 + 0.5F;
-        float f3 = 0.52F;
-        float f4 = par5Random.nextFloat() * 0.6F - 0.3F;
+            this.sparkle(world, par2, par3, par4);
+    }
 
-        if (l == 4)
-        {
-            par1World.spawnParticle("smoke", (double)(f - f3), (double)f1, (double)(f2 + f4), 0.0D, 0.0D, 0.0D);
-            par1World.spawnParticle("flame", (double)(f - f3), (double)f1, (double)(f2 + f4), 0.0D, 0.0D, 0.0D);
-        }
-        else if (l == 5)
-        {
-            par1World.spawnParticle("smoke", (double)(f + f3), (double)f1, (double)(f2 + f4), 0.0D, 0.0D, 0.0D);
-            par1World.spawnParticle("flame", (double)(f + f3), (double)f1, (double)(f2 + f4), 0.0D, 0.0D, 0.0D);
-        }
-        else if (l == 2)
-        {
-            par1World.spawnParticle("smoke", (double)(f + f4), (double)f1, (double)(f2 - f3), 0.0D, 0.0D, 0.0D);
-            par1World.spawnParticle("flame", (double)(f + f4), (double)f1, (double)(f2 - f3), 0.0D, 0.0D, 0.0D);
-        }
-        else if (l == 3)
-        {
-            par1World.spawnParticle("smoke", (double)(f + f4), (double)f1, (double)(f2 + f3), 0.0D, 0.0D, 0.0D);
-            par1World.spawnParticle("flame", (double)(f + f4), (double)f1, (double)(f2 + f3), 0.0D, 0.0D, 0.0D);
-        }
+    private void sparkle(World world, int par2, int par3, int par4)
+    {
+            Random random = world.rand;
+            double d0 = 0.0625D;
+            for(int i = 0; i < 4; i++)
+            {
+                    double d1 = (double)((float)par2 + random.nextFloat());
+                    double d2 = (double)((float)par3 + random.nextFloat());
+                    double d3 = (double)((float)par4 + random.nextFloat());
+
+                    if(i == 0 && !world.isBlockOpaqueCube(par2, par3 + 1, par4))
+                    {
+                            d2 = (double)(par3 + 1) + d0;
+                    }
+
+                    if(i == 1 && !world.isBlockOpaqueCube(par2, par3 - 1, par4))
+                    {
+                            d2 = (double)(par3 + 0) - d0;
+                    }
+
+                    if(i == 2 && !world.isBlockOpaqueCube(par2, par3, par4 + 1))
+                    {
+                            d3 = (double)(par4 + 1) + d0;
+                    }
+
+                    if(i == 3 && !world.isBlockOpaqueCube(par2, par3, par4 - 1))
+                    {
+                            d3 = (double)(par4 + 0) - d0;
+                    }
+
+                    if(i == 4 && !world.isBlockOpaqueCube(par2 + 1, par3, par4))
+                    {
+                            d1 = (double)(par2 + 1) + d0;
+                    }
+
+                    if(i == 5 && !world.isBlockOpaqueCube(par2 - 1, par3, par4))
+                    {
+                            d1 = (double)(par2 + 0) - d0;
+                    }
+
+                    if (d1 < (double)par2 || d1 > (double)(par2 + 1) || d2 < 0.0D || d2 > (double)(par3 + 1) || d3 < (double)par4 || d3 > (double)(par4 + 1))
+                    {
+                    	world.spawnParticle("smoke", d1, d2, d3, 0.0D, 0.0D, 0.0D);
+
+                    }
+
+            }
+
     }
 }
