e429dc37405c4180aa192764ed319aff6bf56643
Gabriel <ironhammer354@gmail.com>
2014-02-03 08:12:58 -0800

    Making Pretty Blocks! Part:1

--- common/com/ironhammer354/mythic/block/WitherBlock.java
+++ common/com/ironhammer354/mythic/block/WitherBlock.java
@@ -1,10 +1,13 @@
 package com.ironhammer354.mythic.block;
 
+import java.util.Random;
+
 import javax.swing.Icon;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.world.World;
 
 import com.ironhammer354.mythic.Mythic;
 import com.ironhammer354.mythic.lib.Reference;
@@ -25,6 +28,7 @@ public class WitherBlock extends Block {
 		setUnlocalizedName(Strings.WITHERBLOCK_NAME);
 		setHardness(3.0F);
 		setResistance(8.0F);
+		setTickRandomly(true);
 	}
 	
 	@SideOnly(Side.CLIENT)
@@ -38,5 +42,36 @@ public class WitherBlock extends Block {
     {
 
         return unlocalizedName.substring(unlocalizedName.indexOf(".") + 1);
+    }
+	
+	public void randomDisplayTick(World par1World, int par2, int par3, int par4, Random par5Random)
+    {
+		int l = par1World.getBlockMetadata(par2, par3, par4);
+        float f = (float)par2 + 0.5F;
+        float f1 = (float)par3 + 0.0F + par5Random.nextFloat() * 6.0F / 16.0F;
+        float f2 = (float)par4 + 0.5F;
+        float f3 = 0.52F;
+        float f4 = par5Random.nextFloat() * 0.6F - 0.3F;
+
+        if (l == 4)
+        {
+            par1World.spawnParticle("smoke", (double)(f - f3), (double)f1, (double)(f2 + f4), 0.0D, 0.0D, 0.0D);
+            par1World.spawnParticle("flame", (double)(f - f3), (double)f1, (double)(f2 + f4), 0.0D, 0.0D, 0.0D);
+        }
+        else if (l == 5)
+        {
+            par1World.spawnParticle("smoke", (double)(f + f3), (double)f1, (double)(f2 + f4), 0.0D, 0.0D, 0.0D);
+            par1World.spawnParticle("flame", (double)(f + f3), (double)f1, (double)(f2 + f4), 0.0D, 0.0D, 0.0D);
+        }
+        else if (l == 2)
+        {
+            par1World.spawnParticle("smoke", (double)(f + f4), (double)f1, (double)(f2 - f3), 0.0D, 0.0D, 0.0D);
+            par1World.spawnParticle("flame", (double)(f + f4), (double)f1, (double)(f2 - f3), 0.0D, 0.0D, 0.0D);
+        }
+        else if (l == 3)
+        {
+            par1World.spawnParticle("smoke", (double)(f + f4), (double)f1, (double)(f2 + f3), 0.0D, 0.0D, 0.0D);
+            par1World.spawnParticle("flame", (double)(f + f4), (double)f1, (double)(f2 + f3), 0.0D, 0.0D, 0.0D);
+        }
     }
 }
