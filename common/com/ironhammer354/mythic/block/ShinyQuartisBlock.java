68c2d5af1dc90c97116849c33df5e0c215dff5be
Gabriel <ironhammer354@gmail.com>
2014-02-05 17:00:52 -0800

    Custom Dimension. Part:3
    DO NOT USE

--- common/com/ironhammer354/mythic/block/ShinyQuartisBlock.java
+++ common/com/ironhammer354/mythic/block/ShinyQuartisBlock.java
@@ -4,16 +4,10 @@ import javax.swing.Icon;
 
 import net.minecraft.block.BlockPortal;
 import net.minecraft.client.renderer.texture.IconRegister;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.src.ModLoader;
-import net.minecraft.world.World;
 
 import com.ironhammer354.mythic.Mythic;
 import com.ironhammer354.mythic.lib.Reference;
 import com.ironhammer354.mythic.lib.Strings;
-import com.ironhammer354.mythic.world.gen.mythicworld.MythTeleport;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -32,31 +26,6 @@ public class ShinyQuartisBlock  extends BlockPortal {
 		setResistance(8.0F);
 		setLightValue(0.1F);
 	}
-	  public void onEntityCollidedWithBlock(World par1World, int par2, int par3, int par4, Entity par5Entity)
-      {
-             if ((par5Entity.ridingEntity == null) && (par5Entity.riddenByEntity == null) && ((par5Entity instanceof EntityPlayerMP)))
-             {
-                    EntityPlayerMP player = (EntityPlayerMP) par5Entity;
-                    ModLoader.getMinecraftServerInstance();
-                    MinecraftServer mServer = MinecraftServer.getServer();
-
-                    if (player.timeUntilPortal > 0)
-                    {
-                          player.timeUntilPortal = 10;
-                    }
-                    else if (player.dimension != Mythic.myth1Id)
-                    {
-                          player.timeUntilPortal = 10;
-
-                          player.mcServer.getConfigurationManager().transferPlayerToDimension(player, Mythic.myth1Id, new MythTeleport (mServer.worldServerForDimension(Mythic.myth1Id)));
-                    }
-                    else
-                    {
-                          player.timeUntilPortal = 10;
-                          player.mcServer.getConfigurationManager().transferPlayerToDimension(player, 0, new MythTeleport(mServer.worldServerForDimension(1)));
-                    }
-                    }
-             }
 	@SideOnly(Side.CLIENT)
     public void registerIcons(IconRegister iconRegister)
     {
