b8fea195b6efafd818bd84e3dff1e3cee7844b64
Gabriel <ironhammer354@gmail.com>
2014-02-05 17:01:10 -0800

    Revert "Custom Dimension. Part:1"
    This reverts commit 08b938bdc174d3c3bd712b59680754414638d5ec.

--- common/com/ironhammer354/mythic/block/ShinyQuartisBlock.java
+++ common/com/ironhammer354/mythic/block/ShinyQuartisBlock.java
@@ -1,64 +1,36 @@
 package com.ironhammer354.mythic.block;
 
+import java.util.Random;
+
 import javax.swing.Icon;
 
-import net.minecraft.block.BlockPortal;
+import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.src.ModLoader;
 import net.minecraft.world.World;
 
 import com.ironhammer354.mythic.Mythic;
 import com.ironhammer354.mythic.lib.Reference;
 import com.ironhammer354.mythic.lib.Strings;
-import com.ironhammer354.mythic.world.gen.mythicworld.MythTeleport;
-import com.ironhammer354.mythic.world.gen.mythicworld.MythWorlds;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-public class ShinyQuartisBlock  extends BlockPortal {
+public class ShinyQuartisBlock extends Block {
 
 	@SideOnly(Side.CLIENT)
 	public Icon[] icons;
 
 	public ShinyQuartisBlock(int id) {
 
-		super(id);
+		super(id, Material.rock);
 		setCreativeTab(Mythic.tabMyth);
 		setUnlocalizedName(Strings.SHINYQUARTISBLOCK_NAME);
 		setHardness(3.0F);
 		setResistance(8.0F);
 		setLightValue(0.1F);
 	}
-	  public void onEntityCollidedWithBlock(World par1World, int par2, int par3, int par4, Entity par5Entity)
-      {
-             if ((par5Entity.ridingEntity == null) && (par5Entity.riddenByEntity == null) && ((par5Entity instanceof EntityPlayerMP)))
-             {
-                    EntityPlayerMP player = (EntityPlayerMP) par5Entity;
-                    ModLoader.getMinecraftServerInstance();
-                    MinecraftServer mServer = MinecraftServer.getServer();
-
-                    if (player.timeUntilPortal > 0)
-                    {
-                          player.timeUntilPortal = 10;
-                    }
-                    else if (player.dimension != MythWorlds.myth1ID)
-                    {
-                          player.timeUntilPortal = 10;
-
-                          player.mcServer.getConfigurationManager().transferPlayerToDimension(player, MythWorlds.myth1ID, new MythTeleport (mServer.worldServerForDimension(MythWorlds.myth1ID)));
-                    }
-                    else
-                    {
-                          player.timeUntilPortal = 10;
-                          player.mcServer.getConfigurationManager().transferPlayerToDimension(player, 0, new MythTeleport(mServer.worldServerForDimension(1)));
-                    }
-                    }
-             }
+	
 	@SideOnly(Side.CLIENT)
     public void registerIcons(IconRegister iconRegister)
     {
