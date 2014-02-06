191cb4494c507de4222d43a7945165a61f2bcc2a
Gabriel <ironhammer354@gmail.com>
2014-02-05 17:01:08 -0800

    Revert "Custom Dimension. Part:2"
    This reverts commit 4c8f7cc8ba6bfc7779a2c73feb10dba435de26a4.

--- common/com/ironhammer354/mythic/Mythic.java
+++ common/com/ironhammer354/mythic/Mythic.java
@@ -5,7 +5,6 @@ import java.io.File;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EnumCreatureType;
 import net.minecraft.world.biome.BiomeGenBase;
-import net.minecraftforge.common.DimensionManager;
 
 import com.ironhammer354.mythic.block.MythBlocks;
 import com.ironhammer354.mythic.config.ConfigurationHandler;
@@ -18,7 +17,6 @@ import com.ironhammer354.mythic.item.MythItems;
 import com.ironhammer354.mythic.lib.Reference;
 import com.ironhammer354.mythic.recipe.CraftingManager;
 import com.ironhammer354.mythic.world.gen.WorldGen;
-import com.ironhammer354.mythic.world.gen.mythicworld.MythWorld1;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
@@ -36,7 +34,6 @@ import cpw.mods.fml.common.registry.LanguageRegistry;
 @NetworkMod(clientSideRequired = true, serverSideRequired = false)
 
 public class Mythic {
-	public static int myth1Id = 8;
 
 	@Instance(Reference.MOD_ID)
 	public static Mythic instance;
@@ -73,9 +70,6 @@ public class Mythic {
 	     EntityRegistry.addSpawn(EntityNymph.class, 17, 2, 8, EnumCreatureType.creature,
                     BiomeGenBase.forest, BiomeGenBase.river);
 		 LanguageRegistry.instance().addStringLocalization("entity.mythic.Nymph.name", "en_US","Nymph");
-		 
-		 DimensionManager.registerProviderType(Mythic.myth1Id, MythWorld1.class, false);
-			DimensionManager.registerDimension(Mythic.myth1Id, Mythic.myth1Id);
 		
 		proxy.registerRenderThings();
 		
