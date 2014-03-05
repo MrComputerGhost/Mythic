9913b9d19b18e0d91761c335252e7398fe4ccede
Gabriel <ironhammer354@gmail.com>
2014-03-05 09:22:33 -0800

    Fix Update

--- common/com/ironhammer354/mythic/entity/mob/EntityNymph.java
+++ common/com/ironhammer354/mythic/entity/mob/EntityNymph.java
@@ -4,10 +4,7 @@ package com.ironhammer354.mythic.entity.mob;
 import net.minecraft.entity.ai.*;
 import net.minecraft.entity.*;
 import net.minecraft.world.*;
-<<<<<<< HEAD
-=======
 import net.minecraft.entity.monster.EntityMob;
->>>>>>> origin/Development
 import net.minecraft.entity.player.*;
 import net.minecraft.item.*;
 
